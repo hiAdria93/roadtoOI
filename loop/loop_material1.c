f5d284e1c49178b9bec477278378f3e6ac5c5d6b
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-04-19 23:22:51 +0800

    Add files via upload

---
+++ loop/loop_material1.c
@@ -0,0 +1,11 @@
+\\列印五次hello world
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    int i;
+    for(i=0;i<5;i++){
+        printf("Hello, World!\n");
+    }
+    return 0;
+}
