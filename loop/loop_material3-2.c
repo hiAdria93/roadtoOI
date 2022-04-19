b4f6dea8024a8242605b8fe66a1b9bac9005c1e6
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-04-20 01:01:52 +0800

    Add files via upload

---
+++ loop/loop_material3-2.c
@@ -0,0 +1,18 @@
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    int a,b,sum=0,i,big,small;
+    scanf("%d %d\n",&a,&b);
+    big=a>b?a:b;
+    small=a<b?a:b;
+    for(i=small;i<=big;i++){
+        if(i%3!=0){
+            continue;
+        }
+        sum=sum+i;
+    }
+    printf("%d",sum);
+    return 0;
+}
