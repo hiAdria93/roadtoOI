c7c6bbce3e536b689cfd1bf423f97dc1762daa8f
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-04-20 10:06:21 +0800

    Add files via upload

---
+++ loop/loop_material4.c
@@ -0,0 +1,39 @@
+//找lcm lcf
+#include <stdio.h>
+#include <stdlib.h>
+int lcm(int,int);
+int lcf(int,int);
+
+int main(int argc, const char * argv[]) {
+    int a, b;
+    scanf("%d %d \n",&a,&b);
+    printf("%d \n %d \n",lcm(a,b),lcf(a,b));
+    return 0;
+}
+
+int lcm(int a, int b){
+    int big,small,i,lc;
+    big=a>b?a:b;
+    small=a<b?a:b;
+    i=big;
+    while(1){
+        if(!(i%a)&&!(i%b)){
+            lc=i;
+            break;
+        }
+        i++;
+    }
+    return lc;
+}
+
+int lcf(int a,int b){
+    int lf=1,big,small,i;
+    big=a>b?a:b;
+    small=a<b?a:b;
+    for(i=1;i<=small;i++){
+        if(!(a%i)&&!(b%i)){
+            lf=i;
+        }
+    }
+    return lf;
+}
