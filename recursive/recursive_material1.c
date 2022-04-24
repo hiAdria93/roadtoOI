4c7a88eb676569dace5a492d35dd202e4e831d0d
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-04-24 18:57:14 +0800

    Update recursive_material1.c

--- recursive/recursive_material1.c
+++ recursive/recursive_material1.c
@@ -1,1 +1,16 @@
+//正整數階乘 遞迴關係式
+#include <stdio.h>
+#include <stdlib.h>
+int factorial(int);
 
+int main(int argc, const char * argv[]){
+  int n;
+  scanf("%d",&n);
+  printf("%d",factorial(n));
+  return 0;
+}
+
+int factorial(register int a){
+  if(a==1)return 1;
+  return a*factorial(a-1);
+}
