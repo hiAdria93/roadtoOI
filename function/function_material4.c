9d3172425511e20c2226d7f50d126f1b15faad79
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-04-26 01:39:26 +0800

    Create function_material4.c

---
+++ function/function_material4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, const char*argv[]){
+  int i,guess,big=1000,small=1,rm=rand()%1000;
+  printf("%d ~ %d\n",small,big);
+  for(i=1;i<=10;i++){
+    if(big!=rm&&small!=rm){
+      scanf("%d\n",&guess);
+      if(guess<rm){
+        small=guess;
+      }
+      else if(guess>rm){
+        big=guess;
+      }
+      else{
+        big=guess;
+        small=guess;
+      }
+    }
+    if(big==rm||small==rm){
+      printf("within %d, sol: %d",i,rm);
+      return 0;
+    }
+    printf("%d remaining",10-i);
+  }
+  if(big!=rm&&small!=rm){
+    printf("sol is %d, your last guess is %d",rm,guess);
+  }
+  return 0;
+}
