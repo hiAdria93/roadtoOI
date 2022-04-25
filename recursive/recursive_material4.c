643cf8a7f9d1299bd8259977805baf4106c2da7b
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-04-25 10:23:16 +0800

    Create recursive_material4.c

---
+++ recursive/recursive_material4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int hanoi(int);
+
+int main(int argc, const char* argv[]){
+  int a;
+  scanf("%d\n",&a);
+  printf("%d",hanoi(a));
+  return 0;
+}
+
+int hanoi(int a){
+  if(a==1)return 1;
+  if(a==2)return 3;
+  return 2*hanoi(a-1)+1;
+}
