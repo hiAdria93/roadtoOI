247285f3145098e523f98a18a2b78e15a7357935
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-04-29 00:48:52 +0800

    Create function_material5.c

---
+++ function/function_material5.c
@@ -0,0 +1,10 @@
+//河内塔加强版
+#include <stdio.h>
+#include <stdlib.h>
+int heneita(int);
+
+int main(int argc, const char*argv[]){
+  int i,j,k,a;
+  scanf("how many plates are there? %d \n",&a);
+  
+}
