326d3ebe8d922311dca3506adcf1f5230452ad1b
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-05-02 15:39:39 +0800

    Create function_test001.c

---
+++ function/function_test001.c
@@ -0,0 +1,27 @@
+//預期結果
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void func1(void);
+void func2(int);
+int x=0;
+
+int main(int argc,const char * argv[]){
+  int x=0;
+  func1();
+  func2(x);
+  printf("%d\n",x);
+  return 0;
+}
+
+void func1(void){
+  static int x=0;
+  x++;
+  printf("%d\n",++x);
+}
+
+void func2(int x){
+  x++;
+  printf("%d\n"x);
+}
