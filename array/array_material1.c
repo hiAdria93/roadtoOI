1729976856b989b4992caabab8744fc6acb11d01
Adrianna <102468772+yanAdria95@users.noreply.github.com>
2022-05-03 20:55:33 +0800

    Create array_material1.c

---
+++ array/array_material1.c
@@ -0,0 +1,14 @@
+//學生成績系統
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(int argc,const char * argv[]){
+  int score[10],i,sum=0;
+  for(i=0;i<10;i++){
+    scanf("%d\n",&score[i]);
+    sum=sum+score[i];
+  }
+  printf("sum %d avg %d",sum,sum/10);
+  return 0;
+}
